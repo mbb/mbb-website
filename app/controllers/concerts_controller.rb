4d9757f69b05758ca06c62f408cb9c5b9e3594f1
Andres Jaan Tack <ajtack@gmail.com>
2009-03-10 16:10:52 -0500

    Filled in all the links in the layouts, with controllers to match.

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,3 +1,5 @@
 class ConcertsController < ApplicationController
-	
+	def index
+		redirect_to next_concerts_url
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
-	map.resources :concerts, :only => [], :collection => {:next => :get}
+	map.news 'news', :controller => 'news', :action => :index
+	map.book 'book', :controller => 'book', :action => :index
+	map.join 'join', :controller => 'join', :action => :index
+	map.resources :players, :only => [:index]
+	map.resources :concerts, :only => [:index], :collection => {:next => :get}
 	map.home 'home', :controller => 'home'
 	map.root :home
 end
