b3b5f06977cefb144ea8b9d426a72aeebad9f687
Andres Jaan Tack <ajtack@gmail.com>
2009-03-16 21:40:08 -0500

    Incorporated board advice, added history section and changed language for joining.
    Also:
    - Replaced text on home page with an excerpt from Nate's recent ad flier.
    - Added "Home" and "History" sections to the menu.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
-	map.news 'news', :controller => 'news', :action => :index
-	map.book 'book', :controller => 'book', :action => :index
-	map.join 'join', :controller => 'join', :action => :index
+	map.history 'history', :controller => 'history'
+	map.news 'news', :controller => 'news'
+	map.book 'book', :controller => 'book'
+	map.join 'join', :controller => 'join'
 	map.resources :players, :only => [:index]
 	map.resources :concerts, :only => [:index], :collection => {:next => :get}
 	map.home 'home', :controller => 'home'
