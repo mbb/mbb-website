e9dc6110a863f077a137686efa4a4d2a608190d5
Andres Jaan Tack <ajtack@gmail.com>
2009-05-26 01:18:31 -0500

    Fixed the spec to succeed. These helpers, however, might deserve a rename.

--- spec/helpers/members_helper_spec.rb
+++ spec/helpers/members_helper_spec.rb
@@ -24,7 +24,7 @@ describe MembersHelper do
       lambda { link_to_member(nil) }.should raise_error('Invalid member')
     end
     it "should link to the given member" do
-      should_receive(:member_path).at_least(:once).and_return('/members/1')
+      should_receive(:private_member_path).at_least(:once).and_return('/members/1')
       link_to_member(@member).should have_tag("a[href='/members/1']")
     end
     it "should use given link text if :content_text is specified" do
@@ -84,7 +84,7 @@ describe MembersHelper do
       stub!(:current_member).and_return(@member)
     end
     it "should link to the given member" do
-      should_receive(:member_path).at_least(:once).and_return('/members/1')
+      should_receive(:private_member_path).at_least(:once).and_return('/members/1')
       link_to_current_member().should have_tag("a[href='/members/1']")
     end
     it "should use given link text if :content_text is specified" do
