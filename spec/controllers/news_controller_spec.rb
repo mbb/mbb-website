ba22106e158e544454515050b436b9d1fef1104a
Andres Jaan Tack <ajtack@gmail.com>
2009-05-26 22:24:56 -0500

    Implemented a database-driven news controller so we stop losing old stories when I update the code.

---
+++ spec/controllers/news_controller_spec.rb
@@ -0,0 +1,25 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe NewsController do
+	it { should route(:get, '/news').to(:controller => :news, :action => :index) }
+	
+	describe 'index' do
+		fixtures :stories
+		
+		before :each do
+			get :index
+		end
+		
+		it 'should expose an array of stories in @stories' do
+			assigns(:stories).class.should be(Array)
+		end
+		
+		it 'should expose only ten stories in @stories' do
+			assigns(:stories).length.should equal(10)
+		end
+		
+		it 'should expose the very last ten stories in @stories' do
+			assigns(:stories).should eql(Story.find(:all, :order => 'date DESC', :limit => 10))
+		end
+	end
+end
---
+++ spec/models/story_spec.rb
@@ -0,0 +1,11 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Story do
+	it { should have_db_column(:title) }
+	it { should have_db_column(:date) }
+	it { should have_db_column(:body) }
+	
+	it { should validate_presence_of(:title) }
+	it { should validate_presence_of(:date) }
+	it { should validate_presence_of(:body) }
+end
