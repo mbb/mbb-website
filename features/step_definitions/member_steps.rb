3c6b4b5c8265e2d94c0a49bb75393d2960fd0d1d
Andres Jaan Tack <ajtack@gmail.com>
2009-05-26 21:05:35 -0500

    Added a new feature: board members get to add new members!
    Feature includes story and implementation.

--- features/step_definitions/member_steps.rb
+++ features/step_definitions/member_steps.rb
@@ -78,7 +78,6 @@ Then "$actor should be logged in" do |name|
 end
 
 Then '$actor should have the default password' do |member_name|
-	debugger
 	member = Member.find_by_name(member_name)
 	member.should_not be_nil
 	member.password_is_temporary.should be(true)
