c915cd3c5869fb6bd5627708466528df9caf47b1
Andres Jaan Tack <ajtack@gmail.com>
2009-10-28 18:19:09 -0500

    Added separate deployment paths for staging and production. Also fixed the nonexistent log-file problem.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,44 @@
-set :application, 'mbb-test'
-set :user, 'minusuu'
-set :domain, 'mbb-test.minusuu.com'
-set :deploy_to, '/home/minusuu/sites/mbb-test.minusuu.com'
-set :server_hostname, 'minusuu.com'
+# Methods of deployment. One will deploy to the test server, the other deploys to
+# production. They should be used as:
+#   $ cap production deploy
+task :production do
+  set :domain, 'madisonbrass.com'
+  use_minusuu
+end
 
+task :staging do
+  set :domain, 'mbb-test.minusuu.com'
+  set :keep_releases, 1
+  use_minusuu
+end
+
+# Sets up to use Tack's server at minusuu.com for the deployment.
+task :use_minusuu do
+  set :user, 'minusuu'
+  set :server_hostname, 'minusuu.com'
+  set :deploy_to, "/home/#{user}/sites/#{domain}"
+  role :app, server_hostname
+  role :web, server_hostname
+  role :db,  server_hostname, :primary => true
+end
+
+# Point to the code repository wherefrom to get the stuff.
 set :scm, 'git'
 set :repository, 'git://github.com/ajtack/mbb.git'
 ssh_options[:forward_agent] = true
 set :branch, 'master'
-set :deploy_via, :remote_cache
 set :git_shallow_clone, 1
 set :git_enable_submodules, 1
-set :git_update_submodules, 1
 set :use_sudo, false
-set :deploy_to, "/home/#{user}/sites/#{domain}"
+set :deploy_via, :remote_cache
 
-role :app, server_hostname
-role :web, server_hostname
-role :db,  server_hostname, :primary => true
-
+before 'deploy:cold' do
+  run "mkdir -p #{deploy_to}/shared/log"
+  run "touch #{deploy_to}/shared/log/development.log"
+  run "touch #{deploy_to}/shared/log/test.log"
+  run "touch #{deploy_to}/shared/log/production.log"
+  run "chmod 0666 #{deploy_to}/shared/log/*.log"
+end
 after 'deploy', 'deploy:cleanup'
 after 'deploy:update_code', 'deploy:symlink_db'
 
