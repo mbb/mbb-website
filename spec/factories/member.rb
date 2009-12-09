19d43dd690fcfc2d648553d9a3d208d4cc1df751
Andres Jaan Tack <ajtack@gmail.com>
2009-12-09 12:04:49 -0600

    Made a factory for Member and Section.

---
+++ spec/factories/member.rb
@@ -0,0 +1,6 @@
+Factory.define :member do |m|
+	m.sequence(:name) { |number| "User ##{number}" }
+	m.sequence(:email) { |number| "user_#{number}@mbb.com" }
+	m.sequence(:phone_number) { |number| "212-436-#{number.to_s.rjust(4, "0")}" }
+	m.association :section
+end
