8b77294fc9cad5e8e257ce8fd4908b7d6b939e33
Andres Jaan Tack <ajtack@gmail.com>
2009-12-19 20:15:03 -0600

    Wrote a spec for upcoming concerts.

--- spec/models/concert_spec.rb
+++ spec/models/concert_spec.rb
@@ -7,7 +7,33 @@ describe Concert do
   it { should validate_presence_of(:title) }
   it { should_not validate_presence_of(:description) }
   
-  it 'should expose #upcoming concerts'
+  describe '#upcoming' do
+    it 'should expose future concerts' do
+      upcoming_concerts = [
+        Factory(:concert, :date => 2.days.from_now),
+        Factory(:concert, :date => 3.days.from_now)
+      ]
+      upcoming_concerts.each do |this_concert|
+        Concert.upcoming.should include(this_concert)        
+      end
+    end
+
+    it 'should not expose past concerts' do
+      past_concerts = [
+        Factory(:concert, :date => 2.days.ago),
+        Factory(:concert, :date => 3.days.ago)
+      ]
+      past_concerts.each do |this_concert|
+        Concert.upcoming.should_not include(this_concert)        
+      end
+    end
+    
+    it 'should display concerts that happened earlier today as upcoming' do
+      todays_concert = Factory(:concert, :date => Date.today, :time => 1.minute.ago)
+      Concert.upcoming.should include(todays_concert)
+    end
+  end
+  
   it 'should expose #past concerts'
   it 'should expose the #next concert'
 end
