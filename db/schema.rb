d097ec977f4dd0ae79159602dd33a3b62ffa226a
Andres Jaan Tack <ajtack@gmail.com>
2010-01-18 14:42:45 -0600

    Added a migration to automatically generate slugs when deploying.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091209060011) do
+ActiveRecord::Schema.define(:version => 20100118203426) do
 
   create_table "attached_files", :force => true do |t|
     t.string   "data_file_name"
