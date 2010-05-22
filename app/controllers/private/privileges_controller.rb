5a11e99c683fbce4e3fbf6321a37b0493396e806
Andres Jaan Tack <ajtack@gmail.com>
2010-05-22 13:44:36 -0500

    Implemented the "What can I do?" page.
    Dramatically rearranged tests for privileges; the old way was backwards.

--- app/controllers/private/privileges_controller.rb
+++ app/controllers/private/privileges_controller.rb
@@ -1,4 +1,8 @@
 class Private::PrivilegesController < PrivateController
 	before_filter :require_member_edit_credentials, :only => [:show]
 	before_filter :require_privileges, :only => [:edit]
+	
+	def show
+		@member = Member.find(params[:member_id])
+	end
 end
--- spec/controllers/privileges_controller_spec.rb
+++ spec/controllers/privileges_controller_spec.rb
@@ -4,32 +4,23 @@ describe Private::PrivilegesController do
 	it { should route(:get, '/private/privileges/edit').to(:controller => 'private/privileges', :action => :edit) }
 	it { should route(:get, '/members/1/privileges').to(:controller => 'private/privileges', :action => :show, :member_id => 1) }
 	
-	context 'when not logged in' do
-		before :each do
-			logout
-		end
-		
-		describe '#edit' do
+	describe '#edit' do
+		context 'when not logged in' do
+			before :each do
+				logout
+			end
+			
 			it 'should redirect to the login page' do
 				get :edit
 				response.should redirect_to(login_url)
 			end
 		end
 		
-		describe '#show' do
-			it 'should redirect to the login page' do
-				get :edit
-				response.should redirect_to(login_url)
+		context 'when logged in as an unprivileged member' do
+			before :each do
+				login({}, {:privileged => false})
 			end
-		end
-	end
-
-	context 'when logged in as an unprivileged member' do
-		before :each do
-			login({}, {:privileged => false})
-		end
-		
-		describe '#edit' do
+			
 			it 'should deny access' do
 				get :edit
 				response.code.should == '403' # Forbidden
@@ -41,7 +32,41 @@ describe Private::PrivilegesController do
 			end
 		end
 		
-		describe '#show (for himself)' do
+		context 'when logged in as a privileged member' do
+			before :each do
+				login({}, {:privileged => true})
+			end
+			
+			it 'should permit access' do
+				get :edit
+				response.should be_success
+			end
+	
+			it 'should render the page' do
+				get :edit
+				response.should render_template('private/privileges/edit')
+			end
+		end
+	end
+	
+	describe '#show (for yourself)' do
+		context 'when not logged in' do
+			before :each do
+				logout
+			end
+			
+			it 'should redirect to the login page' do
+				get :edit
+				response.should redirect_to(login_url)
+			end
+		end
+		
+		context 'when logged in as an unprivileged member' do
+			before :each do
+				login({}, {:privileged => false})
+				Member.should_receive(:find).with(current_user.id.to_s).and_return(current_user)
+			end
+			
 			it 'should permit access' do
 				get :show, :member_id => current_user.id
 				response.should be_success
@@ -51,11 +76,56 @@ describe Private::PrivilegesController do
 				get :show, :member_id => current_user.id
 				response.should render_template('private/privileges/show')
 			end
+			
+			it 'should set the :member variable identifying him' do
+				Member.stub!(:find).with(current_user.id).and_return(current_user)
+				get :show, :member_id => current_user.id
+				assigns(:member).should == current_user
+			end
 		end
 		
-		describe '#show (for another member)' do
+		context 'when logged in as a privileged member' do
 			before :each do
-				@other_member = Factory.create(:member)
+				login({}, {:privileged => true})
+				Member.should_receive(:find).with(current_user.id.to_s).and_return(current_user)
+			end
+			
+			it 'should permit access' do
+				get :show, :member_id => current_user.id
+				response.should be_success
+			end
+			
+			it 'should render the page' do
+				get :show, :member_id => current_user.id
+				response.should render_template('private/privileges/show')
+			end
+			
+			it 'should set the :member variable identifying him' do
+				get :show, :member_id => current_user.id
+				assigns(:member).should == current_user
+			end
+		end
+	end
+	
+	describe '#show (for another member)' do
+		before :each do
+			@other_member = stub_model(Member)
+		end
+		
+		context 'when not logged in' do
+			before :each do
+				logout
+			end
+			
+			it 'should redirect to the login page' do
+				get :edit
+				response.should redirect_to(login_url)
+			end
+		end
+		
+		context 'when logged in as an unprivileged member' do
+			before :each do
+				login({}, :privileged => false)
 			end
 			
 			it 'should deny access' do
@@ -68,40 +138,11 @@ describe Private::PrivilegesController do
 				response.should_not render_template('private/privileges/show')
 			end
 		end
-	end
-
-	context 'when logged in as a privileged member' do
-		before :each do
-			login({}, {:privileged => true})
-		end
 		
-		describe '#edit' do
-			it 'should permit access' do
-				get :edit
-				response.should be_success
-			end
-	
-			it 'should render the page' do
-				get :edit
-				response.should render_template('private/privileges/edit')
-			end
-		end
-		
-		describe '#show (for himself)' do
-			it 'should permit access' do
-				get :show, :member_id => current_user.id
-				response.should be_success
-			end
-			
-			it 'should render the page' do
-				get :show, :member_id => current_user.id
-				response.should render_template('private/privileges/show')
-			end
-		end
-		
-		describe '#show (for another member)' do
+		context 'when logged in as a privileged member' do
 			before :each do
-				@other_member = Factory.create(:member)
+				login({}, {:privileged => true})
+				Member.should_receive(:find).with(@other_member.id.to_s).and_return(@other_member)
 			end
 			
 			it 'should permit access' do
@@ -112,6 +153,11 @@ describe Private::PrivilegesController do
 			it 'should render the page' do
 				get :show, :member_id => @other_member.id
 				response.should render_template('private/privileges/show')
+			end
+			
+			it 'should set the :member variable identifying the target member' do
+				get :show, :member_id => @other_member.id
+				assigns(:member).should == @other_member
 			end
 		end
 	end
