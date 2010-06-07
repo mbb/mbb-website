b56501ac60619e701b009039b6e3953ed46ce3d3
Andres Jaan Tack <ajtack@gmail.com>
2010-06-07 12:44:10 -0500

    Added drag/drop functionality, minus a callback to the server.

--- public/javascripts/views/private/rosters/show.js
+++ public/javascripts/views/private/rosters/show.js
@@ -3,8 +3,39 @@ $(function() {
 });
 
 function roster_effects_setup() {
+	prepare_draggable_members();
 	prepare_updown_buttons();
 };
+
+
+/*
+ * Members should be sortable within sections and between sections.
+ */
+function prepare_draggable_members() {
+	// The user interface should support this by dragging and dropping.
+	$('.section').sortable({
+		connectWith: '.section',
+		cursor: "move",
+		forceHelperSize: true,
+		opacity: 0.6,
+		stop: refresh_updown_buttons  // Fix the buttons, which must surely have changed.
+	});
+	
+	// Show a hint to the user.
+	// Dragging is javascript-only functionality, else this would be fine in CSS.
+	$('.hint').addClass("instructions");
+	$('.member').mouseenter(function() { $(this).find('.hint').css('visibility', 'visible'); });
+	$('.member').mouseleave(function() { $(this).find('.hint').css('visibility', 'hidden' ); });
+}
+
+
+function refresh_updown_buttons() {
+	$('li.member:first-child .member-up-button'  ).button("disable");
+	$('li.member:last-child  .member-down-button').button("disable");
+	$('li.member:not(li.member:first-child) .member-up-button'  ).button("enable");
+	$('li.member:not(li.member:last-child)  .member-down-button').button("enable");
+}
+
 
 /*
  * If users prefer to click, allow up/down buttons to work with AJAX.
